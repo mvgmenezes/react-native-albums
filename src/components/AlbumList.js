ed79b3f8768d45ff09a18dd3922abf14a15ddaab
Marcus Menezes <mvgmenezes@gmail.com>
2019-02-03 12:20:17 -0200

    Alterando o componente Album list  para uma classe para obter dados

---
+++ src/components/AlbumList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+
+
+class AlmbumList extends Component{
+
+    render(){
+        return (
+            <View>
+                <Text> Album List!!! </Text>
+            </View>
+        );
+    }
+    
+}
+
+export default AlbumList;
