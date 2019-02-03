3dc8cff7f1e38b658e9b0a77b6664d6bccd034b2
Marcus Menezes <mvgmenezes@gmail.com>
2019-02-03 13:19:50 -0200

    Recuperando os dados do JSON e apresentando

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 
 
-class AlmbumList extends Component{
+class AlbumList extends Component{
+
+    //resolvendo o problema do método ser assincrono, crianod uma variavel state
+    state = { albums: []};
+
+    //ciclo de vida do component, vai ser chamado quando esta classe for renderizada
+    //carregaremos os dados quando o usuario acessar a lista
+    componentWillMount(){
+
+         //fetch('https://rallycoding.herokuapp.com/api/music_albums')
+        //.then((response) => response.json())
+        //.then((responseData) => {
+        //    console.log(responseData);
+        //    debugger;
+        //});
+
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => this.setState({albums: response.data}));
+
+       
+    }
+
+    renderAlbums(){
+        //map ja realiza a interacao
+        return this.state.albums.map(album => <Text>{album.title}</Text>)
+    }
 
     render(){
+        console.log(this.state);
+
         return (
             <View>
-                <Text> Album List!!! </Text>
+                {this.renderAlbums()}
             </View>
         );
     }
